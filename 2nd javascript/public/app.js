7faff7940fa1ad1ed39947f83aef471f3e0d52f9
Aahad Ali <104500845+Aahad-Ali@users.noreply.github.com>
2022-07-31 04:10:59 -0700

    Add files via upload

---
+++ 2nd javascript/public/app.js
@@ -0,0 +1,21 @@
+
+var username= "My name is Aahad Ali";
+alert(username);
+var age= "18 Years old";
+alert(age);
+var depart = "WMAD";
+alert(depart);
+var course = "Mobile & cloud Computing : Module A";
+alert(course);
+var gift = "Aahad" + "\n" + "Aaha" + "\n" + "Aah" + "\n" + "Aa" + "\n" + "A";
+alert(gift);
+var success = "INSHALLAH one day I will be a top world best softwere developer in the world ";
+alert(success);
+var email = "aahadrajput69@gmail.com its my email";
+alert(email);
+var education = "I am learning javascript from tha book 'A smarter way to learn javascript'";
+alert(education);
+
+
+
+
